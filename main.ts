5acea4a8f90193f08b1e4b938e194fea09647174
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2023-11-21 16:33:36 -0800

    clean up comments

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 import fs from 'fs';
 
+// print debug message
 console.log('hello from typescript');
 
-const lastResults = fs.readFileSync('last-results.txt');
+// get results from previous run
+const lastResults = fs.readFileSync('last-results.txt').toString();
+
+// create results for this run
 const results = `test results content at ${new Date().toLocaleString()}`;
 
+// print debug message
 console.log('last results were', lastResults);
 console.log('new results are', results);
 
+// save results for this run
 fs.writeFileSync('results.txt', results);
