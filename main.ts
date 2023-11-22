ad5bade2b1c434c5cca0a1fb50dfc8ebbdef9d96
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2023-11-21 16:29:47 -0800

    script should now be set up to operate based on the previous results

--- main.ts
+++ main.ts
@@ -2,7 +2,10 @@ import fs from 'fs';
 
 console.log('hello from typescript');
 
-fs.writeFileSync(
-    'results.txt',
-    `test results content at ${new Date().toLocaleString()}`,
-);
+const lastResults = fs.readFileSync('last-results.txt');
+const results = `test results content at ${new Date().toLocaleString()}`;
+
+console.log('last results were', lastResults);
+console.log('new results are', results);
+
+fs.writeFileSync('results.txt', results);
